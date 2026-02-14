a49f86e067e8406dc38174196d88eb1fa1722ece
Pria Bobby <priab@bu.edu>
2026-02-14 13:58:01 -0500

    Solution for part2

--- homework_3_guessing_game/src/part2.rs
+++ homework_3_guessing_game/src/part2.rs
@@ -6,7 +6,15 @@ pub struct Part2 {}
 // Terrible strategy: ask if the number is min, otherwise return max.
 impl Strategy for Part2 {
     fn guess_the_number(player: &mut Player, min: u32, max: u32) -> u32 {
-        // YOUR SOLUTION GOES HERE.
-        todo!("No solution provided yet")
+        fn recursive(player: &mut Player, low: u32, high: u32) -> u32 {
+            let mid = (high + low) / 2;
+            match player.ask_to_compare(mid) {
+                0 => mid,
+                -1 => recursive(player, low, mid - 1),
+                1 => recursive(player, mid + 1, high),
+                _ => unreachable!("You're cheating!"),
+            }
+        }
+        recursive(player, min, max)
     }
 }
